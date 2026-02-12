204725a10ead8fc24c3c522d9c1dbf33afe49796
tryck <patryckthomas@gmail.com>
2026-02-12 12:26:37 -0500

    chore: enabled swagger(catalog service)

--- Services/CatalogService/CatalogService.API/Controllers/ProductsController.cs
+++ Services/CatalogService/CatalogService.API/Controllers/ProductsController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CatalogService.API.Controllers;
 
+/// <summary>
+/// Manages product catalog.
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class ProductsController : ControllerBase
@@ -13,12 +16,21 @@ public class ProductsController : ControllerBase
     private readonly ILogger<ProductsController> _logger;
     private readonly IMediator _mediator;
 
+    /// <summary>
+    /// Manages product catalog.
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="mediator"></param>
     public ProductsController(ILogger<ProductsController> logger, IMediator mediator)
     {
         _logger = logger;
         _mediator = mediator;
     }
 
+    /// <summary>
+    /// Retreives a list of products.
+    /// </summary>
+    /// <returns></returns>
     [HttpGet]
     public async Task<IActionResult> GetProducts()
     {
@@ -26,6 +38,11 @@ public class ProductsController : ControllerBase
         return Ok(products.Adapt<IEnumerable<ProductDto>>());
     }
 
+    /// <summary>
+    /// Retreives a product by ProductId.
+    /// </summary>
+    /// <param name="productId"></param>
+    /// <returns></returns>
     [HttpGet("{productId}")]
     public async Task<IActionResult> GetProduct(int productId)
     {
--- Services/CatalogService/CatalogService.API/Program.cs
+++ Services/CatalogService/CatalogService.API/Program.cs
@@ -2,17 +2,38 @@ using Web.Middleware;
 using CatalogService.Application;
 using CatalogService.Infrastructure;
 using CatalogService.Infrastructure.DataInitializer;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "Ecommerce Product Catalog API"
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    opt.IncludeXmlComments(xmlPath);
+});
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("CatalogDb") ?? "CatalogDb", true);
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Ecommerce API v1");
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
